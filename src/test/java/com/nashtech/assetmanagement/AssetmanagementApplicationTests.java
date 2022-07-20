e5f87e424dfe0c249deba47cf7a68e7b12b58226
Duc Nguyen <ducnguyen201231@gmail.com>
2022-07-20 17:27:11 +0700

    Init project

---
+++ src/test/java/com/nashtech/assetmanagement/AssetmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nashtech.assetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
