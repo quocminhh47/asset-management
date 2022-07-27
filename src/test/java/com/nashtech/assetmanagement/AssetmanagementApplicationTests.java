e1b2c0e8d133aa4bd4be4b08050bc67cd4d93a4c
Duc Nguyen <ducnguyen201231@gmail.com>
2022-07-27 10:54:13 +0700

    Refactor profile config

--- src/test/java/com/nashtech/assetmanagement/AssetmanagementApplicationTests.java
+++ src/test/java/com/nashtech/assetmanagement/AssetmanagementApplicationTests.java
@@ -2,8 +2,10 @@ package com.nashtech.assetmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
+@Sql(scripts = {"/data_test.sql"})
 class AssetmanagementApplicationTests {
 
 	@Test
