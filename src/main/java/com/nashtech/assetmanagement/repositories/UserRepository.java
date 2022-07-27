f2219bff8e0696c65b2935f9f69b08795800dbb6
quocminhh47 <quocminhh47@gmail.com>
2022-07-27 16:24:25 +0700

    #574-fix-searching-users: now can search with ignore case

--- src/main/java/com/nashtech/assetmanagement/repositories/UserRepository.java
+++ src/main/java/com/nashtech/assetmanagement/repositories/UserRepository.java
@@ -21,10 +21,16 @@ public interface UserRepository extends JpaRepository<Users, String> {
     @Query(value = "select staff_code from users where staff_code LIKE 'SD%'",nativeQuery = true)
     List<String> findAllStaffCode();
 
+//    @Query(value = "SELECT * FROM users u" +
+//            " where ((u.staff_code like %:text%) or" +
+//            " (concat(u.first_name, u.last_name)) like %:text%)" +
+//            "and u.location_id = :location and u.staff_code != :loggedStaffCode",
+//            nativeQuery = true)
+
     @Query(value = "SELECT * FROM users u" +
-            " where (( LOWER(u.staff_code) like %:text%) or" +
-            " (concat(u.first_name, u.last_name)) like %:text%)" +
-            "and u.location_id = :location and u.staff_code != :loggedStaffCode",
+            " where ((LOWER(u.staff_code) like %:text%) or" +
+            " LOWER((concat(u.first_name, u.last_name))) like %:text%)" +
+            "and LOWER(u.location_id) = :location and u.staff_code != :loggedStaffCode",
             nativeQuery = true)
     Page<Users> searchByStaffCodeOrName(@Param("text") String text,
                                         @Param("loggedStaffCode") String loggedStaffCode,
