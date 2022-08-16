087051e0e7849350939bac042409efdea24dfb3a
Duc Nguyen <ducnguyen201231@gmail.com>
2022-08-16 08:59:23 +0000

    feat: add jacoco for make report test coverage

--- src/main/java/com/nashtech/assetmanagement/AssetManagementApplication.java
+++ src/main/java/com/nashtech/assetmanagement/AssetManagementApplication.java
@@ -9,17 +9,4 @@ public class AssetManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(AssetManagementApplication.class, args);
     }
-
-
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*");
-//			}
-//		};
-//	}
-
-
 }
